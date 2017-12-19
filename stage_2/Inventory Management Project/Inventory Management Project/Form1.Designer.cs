c9197c7397890e8b30d4a9fbc8136d4ceae08dd7
Finn Turnbull <slbquista@gmail.com>
2017-12-19 12:24:23 +0000

    Small updates

--- stage_2/Inventory Management Project/Inventory Management Project/Form1.Designer.cs
+++ stage_2/Inventory Management Project/Inventory Management Project/Form1.Designer.cs
@@ -28,20 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tab_control_1 = new System.Windows.Forms.TabControl();
+            this.Production = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.Tab2 = new System.Windows.Forms.TabPage();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.Sales = new System.Windows.Forms.TabPage();
+            this.tabControl3 = new System.Windows.Forms.TabControl();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            this.tab_control_1.SuspendLayout();
+            this.Production.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.Tab2.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.Sales.SuspendLayout();
+            this.tabControl3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tab_control_1
+            // 
+            this.tab_control_1.Controls.Add(this.Production);
+            this.tab_control_1.Controls.Add(this.Tab2);
+            this.tab_control_1.Controls.Add(this.Sales);
+            this.tab_control_1.Location = new System.Drawing.Point(3, 3);
+            this.tab_control_1.Name = "tab_control_1";
+            this.tab_control_1.SelectedIndex = 0;
+            this.tab_control_1.Size = new System.Drawing.Size(1470, 630);
+            this.tab_control_1.TabIndex = 0;
+            // 
+            // Production
+            // 
+            this.Production.Controls.Add(this.tabControl1);
+            this.Production.Location = new System.Drawing.Point(4, 22);
+            this.Production.Name = "Production";
+            this.Production.Padding = new System.Windows.Forms.Padding(3);
+            this.Production.Size = new System.Drawing.Size(1462, 604);
+            this.Production.TabIndex = 0;
+            this.Production.Text = "Production";
+            this.Production.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.ItemSize = new System.Drawing.Size(42, 18);
+            this.tabControl1.Location = new System.Drawing.Point(6, 6);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1450, 592);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1442, 566);
+            this.tabPage5.TabIndex = 0;
+            this.tabPage5.Text = "Insert";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(1442, 566);
+            this.tabPage6.TabIndex = 1;
+            this.tabPage6.Text = "View";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // Tab2
+            // 
+            this.Tab2.Controls.Add(this.tabControl2);
+            this.Tab2.Location = new System.Drawing.Point(4, 22);
+            this.Tab2.Name = "Tab2";
+            this.Tab2.Padding = new System.Windows.Forms.Padding(3);
+            this.Tab2.Size = new System.Drawing.Size(1462, 604);
+            this.Tab2.TabIndex = 1;
+            this.Tab2.Text = "Packaging";
+            this.Tab2.UseVisualStyleBackColor = true;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage1);
+            this.tabControl2.Controls.Add(this.tabPage2);
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Location = new System.Drawing.Point(6, 6);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(1450, 592);
+            this.tabControl2.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1442, 566);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Bottle";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1442, 566);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Label";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1442, 566);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Move";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1442, 566);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "View";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // Sales
+            // 
+            this.Sales.Controls.Add(this.tabControl3);
+            this.Sales.Location = new System.Drawing.Point(4, 22);
+            this.Sales.Name = "Sales";
+            this.Sales.Padding = new System.Windows.Forms.Padding(3);
+            this.Sales.Size = new System.Drawing.Size(1462, 604);
+            this.Sales.TabIndex = 2;
+            this.Sales.Text = "Sales";
+            this.Sales.UseVisualStyleBackColor = true;
+            // 
+            // tabControl3
+            // 
+            this.tabControl3.Controls.Add(this.tabPage7);
+            this.tabControl3.Controls.Add(this.tabPage8);
+            this.tabControl3.Location = new System.Drawing.Point(6, 3);
+            this.tabControl3.Name = "tabControl3";
+            this.tabControl3.SelectedIndex = 0;
+            this.tabControl3.Size = new System.Drawing.Size(1453, 595);
+            this.tabControl3.TabIndex = 0;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(1445, 569);
+            this.tabPage7.TabIndex = 0;
+            this.tabPage7.Text = "Remove";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Location = new System.Drawing.Point(4, 22);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage8.Size = new System.Drawing.Size(1445, 569);
+            this.tabPage8.TabIndex = 1;
+            this.tabPage8.Text = "View";
+            this.tabPage8.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 635);
+            this.ClientSize = new System.Drawing.Size(1476, 635);
+            this.Controls.Add(this.tab_control_1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
+            this.tab_control_1.ResumeLayout(false);
+            this.Production.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.Tab2.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            this.Sales.ResumeLayout(false);
+            this.tabControl3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tab_control_1;
+        private System.Windows.Forms.TabPage Production;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage Tab2;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage Sales;
+        private System.Windows.Forms.TabControl tabControl3;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TabPage tabPage8;
     }
 }
 
