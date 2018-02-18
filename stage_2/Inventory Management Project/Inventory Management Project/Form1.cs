de6be0129174b8b7c4ecaf46ec5440777a8e1e6c
Finn Turnbull <slbquista@gmail.com>
2018-02-18 17:47:29 +0000

    Changes to date.

--- stage_2/Inventory Management Project/Inventory Management Project/Form1.cs
+++ stage_2/Inventory Management Project/Inventory Management Project/Form1.cs
@@ -16,5 +16,16 @@ namespace Inventory_Management_Project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'graded_unitDataSet2.gu_batch' table. You can move, or remove it, as needed.
+            this.gu_batchTableAdapter2.Fill(this.graded_unitDataSet2.gu_batch);
+            // TODO: This line of code loads data into the 'graded_unitDataSet1.gu_batch' table. You can move, or remove it, as needed.
+            this.gu_batchTableAdapter1.Fill(this.graded_unitDataSet1.gu_batch);
+            // TODO: This line of code loads data into the 'graded_unitDataSet.gu_batch' table. You can move, or remove it, as needed.
+            this.gu_batchTableAdapter.Fill(this.graded_unitDataSet.gu_batch);
+
+        }
     }
 }
