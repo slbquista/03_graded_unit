4b2cd1e00efb180904a8d08e21af9b1716fc3c59
slbquista <slbquista@gmail.com>
2018-03-09 00:07:56 +0000

    Updating

--- stage_2/inventory_management_project - Copy/Inventory Management Project/Form1.cs
+++ stage_2/inventory_management_project - Copy/Inventory Management Project/Form1.cs
@@ -21,9 +21,10 @@ namespace Inventory_Management_Project
         {
             //Connection string - note change the drive letter depending on the letter of the USB!
             //Unsure where the connection string settup for the DataSets created using Visual Studio is stored in code form, but it can be changed via "Project > Project Properties > Settings"
-            System.Data.SqlClient.SqlConnection Connect = new SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="E:\SQL Database\graded_unit.mdf";Integrated Security=True;Connect Timeout=30");
-            DataTable dt = new DataTable();
-            System.Data.SqlClient.SqlCommand insert = new System.Data.SqlClient.SqlCommand();
+            
+            //System.Data.SqlClient.SqlConnection Connect = new SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="E:\SQL Database\graded_unit.mdf";Integrated Security=True;Connect Timeout=30");
+            //DataTable dt = new DataTable();
+            //.Data.SqlClient.SqlCommand insert = new System.Data.SqlClient.SqlCommand();
 
             //DataSets created to read data from the database into DataGridViews
             this.gu_batchTableAdapter.Fill(this.graded_unitDataSet6.gu_batch);
